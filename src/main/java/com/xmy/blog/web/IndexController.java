320b2fe9cebe6146dfe2617145c8bbca4c1cbd6b
xumingyang <xumingyang@greatyun.com>
2021-03-07 20:01:37 +0800

    404和500界面，springBoot会主动拦截，404错误直接导向404.html

---
+++ src/main/java/com/xmy/blog/web/IndexController.java
@@ -0,0 +1,15 @@
+package com.xmy.blog.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+//代表它指的是控制器
+public class IndexController {
+    @GetMapping("/")
+    public String Index() {
+        //这样会出现500错误
+//        int i = 9 / 0;
+        return "index";
+    }
+}
