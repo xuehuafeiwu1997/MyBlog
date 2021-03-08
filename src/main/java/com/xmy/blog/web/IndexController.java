25ff99c85e535d7f50b77b37524b55e01ba8bf62
xumingyang <xumingyang@greatyun.com>
2021-03-08 23:01:56 +0800

    使用fragment提供公共元素以及各个界面的界面优化

--- src/main/java/com/xmy/blog/web/IndexController.java
+++ src/main/java/com/xmy/blog/web/IndexController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 //代表它指的是控制器
 public class IndexController {
     @GetMapping("")
-    public String Index() {
+    public String index() {
         //这样会出现500错误
 //        int i = 9 / 0;
 //        String blog = null;
@@ -18,5 +18,10 @@ public class IndexController {
 //        }
         System.out.println("-----index-----------");
         return "index";
+    }
+
+    @GetMapping("/blog")
+    public String blog() {
+        return "blog";
     }
 }
