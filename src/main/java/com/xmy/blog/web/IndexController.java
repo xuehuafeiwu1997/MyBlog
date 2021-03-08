ff0b5000923537c310f643392eb9e26667cd2e04
xumingyang <xumingyang@greatyun.com>
2021-03-08 10:00:01 +0800

    定义一个切面，用来拦截输出相应的日志

--- src/main/java/com/xmy/blog/web/IndexController.java
+++ src/main/java/com/xmy/blog/web/IndexController.java
@@ -3,18 +3,20 @@ package com.xmy.blog.web;
 import com.xmy.blog.NotFounfExcetion;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 //代表它指的是控制器
 public class IndexController {
-    @GetMapping("/")
-    public String Index() {
+    @GetMapping("/{id}/{name}")
+    public String Index(@PathVariable Integer id, @PathVariable String name) {
         //这样会出现500错误
 //        int i = 9 / 0;
-        String blog = null;
-        if (blog == null) {
-            throw new NotFounfExcetion("博客不存在");
-        }
+//        String blog = null;
+//        if (blog == null) {
+//            throw new NotFounfExcetion("博客不存在");
+//        }
+        System.out.println("-----index-----------");
         return "index";
     }
 }
